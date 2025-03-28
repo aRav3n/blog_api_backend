f42a91274caf85c02264e32f2a926f63582069cb
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-03-28 16:50:55 -0300

    build: fix CORS issues

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const router = require("./routes/router");
 const cors = require("cors");
 
 const allowList = [
-  "https://blog-api-frontend-creator.pages.dev/",
-  "https://blog-api-frontend-viewer.pages.dev/",
+  "https://blog-api-frontend-creator.pages.dev",
+  "https://blog-api-frontend-viewer.pages.dev",
 ];
 const corsOptions = {
-  origins: allowList,
+  origin: allowList,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", cors(corsOptions), router);
+app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
